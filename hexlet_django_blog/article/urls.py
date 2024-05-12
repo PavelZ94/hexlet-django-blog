247e43c13faef7ae49523db3ebb0d73c5f7eb845
Павел <zhilinsky.pa@yandex.ru>
2024-05-12 21:21:57 +0300

    trouble with homework

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ArticleView
 
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('<str:tag>/<int:article_id>/', ArticleView.as_view(), name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -9,3 +9,10 @@ class IndexView(View):
     def get(self, request):
           context = {'name': 'article'}
           return render(request, 'index.html', context)
+
+
+class ArticleView(View):
+
+    def index(self, request, *args, **kwargs):
+        return redirect(reverse('article',
+                                kwargs={'tag': 'python', 'article_id': 42}))
