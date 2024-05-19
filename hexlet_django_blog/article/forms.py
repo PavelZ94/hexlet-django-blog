141d57c49c1320d3fa01500aa1dd0afa686701e2
Павел <zhilinsky.pa@yandex.ru>
2024-05-19 19:28:28 +0300

    made  pages with creating and updatind articles

--- hexlet_django_blog/article/forms.py
+++ hexlet_django_blog/article/forms.py
@@ -1,43 +1,50 @@
 from django import forms
-from .forms import CommentArticleForm
+#from .forms import CommentArticleForm
+from .models import Article
 from django.views import View
 from django.shortcuts import render
 from django.db import models
 from django.forms import ModelForm
 
-class CommentArticleForm(forms.Form):
-    content = forms.CharField(label='Комментарий')
+#class CommentArticleForm(forms.Form):
+#    content = forms.CharField(label='Комментарий')
 
 
-class CommentArticleView(View):
+#class CommentArticleView(View):
 
-    def get(self, request, *args, **kwargs):
-        form = CommentArticleForm()
-        return render(request, 'comment.html', {'form': form})
-
-
-    def post(self, request, *args, **kwargs):
-        form = CommentArticleForm(request.POST)
-        if form.is_valid():
-            comment = Comment(
-                name=form.cleaned_data['content'],
-            )
-            comment.save()
+#    def get(self, request, *args, **kwargs):
+#        form = CommentArticleForm()
+#        return render(request, 'comment.html', {'form': form})
 
 
-class ArticleComment(models.Model):
-    content = models.CharField('content', max_length=100)
+#    def post(self, request, *args, **kwargs):
+#        form = CommentArticleForm(request.POST)
+#        if form.is_valid():
+#            comment = Comment(
+#                name=form.cleaned_data['content'],
+#            )
+#            comment.save()
 
 
-class ArticleCommentForm(ModelForm):
+#class ArticleComment(models.Model):
+#    content = models.CharField('content', max_length=100)
+
+
+#class ArticleCommentForm(ModelForm):
+#    class Meta:
+#        model = ArticleComment
+#        fields = ['content']
+
+
+#class ArticleCommentFormView(View):
+
+#    def post(self, request, *args, **kwargs):
+#        form = ArticleCommentForm(request.POST)
+#        if form.is_valid():
+#            form.save()
+
+
+class ArticleForm(ModelForm):
     class Meta:
-        model = ArticleComment
-        fields = ['content']
-
-
-class ArticleCommentFormView(View):
-
-    def post(self, request, *args, **kwargs):
-        form = ArticleCommentForm(request.POST)
-        if form.is_valid():
-            form.save()
+        model = Article
+        fields = ['name', 'body']
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.views import View
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 # Create your views here.
 from hexlet_django_blog.article.models import Article
-
-
+from .forms import ArticleForm
+from django.contrib import messages
 
 
 class IndexView(View):
@@ -26,10 +26,51 @@ class ArticleView(View):
                       context={'article': article, })
 
 
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        messages = ''
+        return render(request,
+                      'articles/create.html',
+                      {'form': form, 'messages': messages})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Статья добавлена')
+            return redirect('articles')
+        messages.error(request, 'При создании статьи произошла ошибка')
+        return render(request,
+                      'articles/create.html',
+                      {'form': form})
 
 
-#def index(request, tags, article_id):
-#    return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
+class ArticleFormEditView(View):
 
-#def home_redirect(request):
-#    return redirect(reverse('article', args=['python', 42]))
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        messages = ''
+        return render(request,
+                      'articles/update.html',
+                      {'form': form,
+                       'article_id': article_id,
+                       'messages': messages})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Статья изменена')
+            return redirect('articles')
+        messages.error(request, 'При редактировании статьи произошла ошибка')
+        return render(request,
+                      'articles/update.html',
+                      {'form': form,
+                       'article_id': article_id,
+                       'messages': messages})
